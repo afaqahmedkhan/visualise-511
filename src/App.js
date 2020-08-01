24dd6ef62f8cd85b7c8a60cfcb81b46a6c510312
Afaq Ahmed Khan <afaq.qwe@gmail.com>
2020-08-02 01:10:55 +0500

    added google maps javascript API to mark stops

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Card } from "antd";
+import { GoogleApiWrapper } from "google-maps-react";
 import React from "react";
 import {
   getLines,
@@ -7,33 +9,58 @@ import {
   getTimeTable,
 } from "./APIs/api";
 import "./App.css";
-import logo from "./logo.svg";
+import { googleAPIKey } from "./IConstants";
 
-function App() {
+function App(props) {
   getOperators("AC");
   getPatterns("AC", "19");
   getTimeTable("AC", "19");
   getLines("SF");
   getStops("SF");
 
+  // state = {
+  //   showingInfoWindow: false,
+  //   activeMarker: {},
+  //   selectedPlace: {},
+  // };
+
+  const onMarkerClick = (props, marker, e) => {
+    // setState({
+    //   selectedPlace: props,
+    //   activeMarker: marker,
+    //   showingInfoWindow: true,
+    // });
+  };
+
+  const onMapClicked = (props) => {
+    // if (state.showingInfoWindow) {
+    //   setState({
+    //     showingInfoWindow: false,
+    //     activeMarker: null,
+    //   });
+    // }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card title="Stops Located on Google Maps" style={{ width: 300 }}>
+        {/* <Map google={props.google} onClick={onMapClicked}>
+          <Marker onClick={onMarkerClick} name={"Current location"} />
+
+          <InfoWindow
+            marker={state.activeMarker}
+            visible={state.showingInfoWindow}
+          >
+            <div>
+              <h1> {state.selectedPlace.name} </h1>
+            </div>
+          </InfoWindow>
+        </Map> */}
+      </Card>
     </div>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: googleAPIKey,
+})(App);
--- src/IConstants.ts
+++ src/IConstants.ts
@@ -1,2 +1,3 @@
 export const authToken: string = "35c70fcd-76e8-4c2f-ae7f-1bcc8caeac78";
 export const baseUrl: string = "http://api.511.org/transit";
+export const googleAPIKey: string = "AIzaSyDWyiTYtu_VEzqcWl3t5iX3JjZ02kp_dp8";
